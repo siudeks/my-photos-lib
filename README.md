3545f6607bbeff1180fc59b34d490fbd167995e0
siudeks <slawomir@siudek.net>
2024-04-12 07:20:09 +0000

    Startable application

---
+++ README.md
@@ -0,0 +1,2 @@
+## Inspirations:
+- [I built an image search engine (Toutube)](https://www.youtube.com/watch?v=mBcBoGhFndY)
--- src/test/java/net/siudek/media/MediaApplicationTests.java
+++ src/test/java/net/siudek/media/MediaApplicationTests.java
@@ -3,7 +3,10 @@ package net.siudek.media;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		// interactive mode stops the test and waits for input
+		"spring.shell.interactive.enabled=false"
+})
 class MediaApplicationTests {
 
 	@Test
